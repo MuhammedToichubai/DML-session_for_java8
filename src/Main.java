c20635d86e9221396e5694f6188416217c05c489
Muhammed <Muhammed>
2023-01-10 07:51:06 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+/**
+* @created      : ${DATE}
+* @author       : ${USER} Asantegin
+* @project_name : $PROJECT_NAME
+**/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
